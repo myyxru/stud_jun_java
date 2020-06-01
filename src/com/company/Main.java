b05c9361ac8fdf951e8e2d6098f5ac1f3cf84cfa
myyxru <66266114+myyxru@users.noreply.github.com>
2020-06-01 21:02:53 +0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 10;
+        int b = 11;
+        System.out.println(a+b);
+    }
+}
