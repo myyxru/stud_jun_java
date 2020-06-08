de90500c8dbb397e8cac4ff41ff8e8a9e6c66d5f
myyxru <66266114+myyxru@users.noreply.github.com>
2020-06-08 20:17:38 +0500

    qwe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,8 @@ public class Main {
         int a = 10;
         int b = 11;
         System.out.println(a+b);
+        System.out.println(a+b);
+        System.out.println(a+b);
+        System.out.println();
     }
 }
