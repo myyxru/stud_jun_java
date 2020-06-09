cd0296fd7b7fceb2390621eacdde648a1f85fe08
COL-95304 <myyxru@gmail.com>
2020-06-09 17:47:48 +0500

    9

---
+++ src/T1/gsm_method.java
@@ -0,0 +1,22 @@
+package T1;
+
+public class gsm_method {
+    double price_litr_gsm100=46.10;
+    double price_litr_gsm200_400=48.90;
+    double price_litr_gsm300=47.50;
+    double rashod_100=12.5;
+    double rashod_200=12;
+    double rashod_300=11.5;
+    double rashod_400=20;
+
+    public  main(String[] args) {
+
+        public double total_gsm (String[]arr){
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(arr[i]);
+
+            }
+            return 0;
+        }
+    }
+}
