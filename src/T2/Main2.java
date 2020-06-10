55db3d2dc8dac418882bab17d52e40b7efabf8df
myyxru <66266114+myyxru@users.noreply.github.com>
2020-06-10 22:34:01 +0500

    qwe

---
+++ src/T2/Main2.java
@@ -0,0 +1,28 @@
+package T2;
+
+public class Main2 {
+    Main2() {
+        int arr[] = arrayCreator(10);
+    }
+    public int[] arrayCreator ( int index){
+        int arr[] = new int[index];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main2 arr2 = new Main2();
+        for (int i:arr2) {
+            System.out.println(arr2[i]);
+        }
+
+
+
+        //public void add ( int a, int index){
+       // }
+
+    }
+}
+
